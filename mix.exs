ec41d484d8fa2332700220b919c53e19b5a286a5
Steven Nunez <steven.nunez@gmail.com>
2015-06-09 20:27:06 -0400

    Add dotenv

--- mix.exs
+++ mix.exs
@@ -14,19 +14,19 @@ defmodule YoutubeAreYouOke.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: app_list(Mix.env)
+      ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp app_list(:dev), do: [:dotenv| app_list]
+  defp app_list(_), do: app_list
+  defp app_list, do: [:logger]
+
   defp deps do
-    [{:exvcr, github: "parroty/exvcr"}]
+    [
+      {:exvcr, github: "parroty/exvcr"},
+      {:dotenv, github: "avdi/dotenv_elixir"}
+    ]
   end
 end
--- test/youtube_are_you_oke_test.exs
+++ test/youtube_are_you_oke_test.exs
@@ -1,5 +1,11 @@
 defmodule YoutubeAreYouOkeTest do
   use ExUnit.Case
+  use ExVCR.Mock
+
+  setup_all do
+    ExVCR.Config.cassette_library_dir("fixture/vcr_cassettes")
+    :ok
+  end
 
   test "the truth" do
     assert 1 + 1 == 2
